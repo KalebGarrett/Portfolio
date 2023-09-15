3e2d7d7c9c8e47c4eeda7909f667737e726ccb49
Kaleb Garrett <kalebagarrett@gmail.com>
2023-09-15 17:24:28 -0400

    Updated skill bars

--- Portfolio.Web/wwwroot/js/site.js
+++ Portfolio.Web/wwwroot/js/site.js
@@ -1,27 +1,26 @@
-﻿$(document).ready(function () {
-    // Iterate through each skill circle
-    $(".skill-circle").each(function () {
-        var skillValue = parseInt($(this).data("value")); // Get the skill value from data attribute
-        var skillColor = calculateSkillColor(skillValue); // Calculate the skill color based on value
-        $(this).css("border-color", skillColor); // Update the circle's border color
-    });
+﻿$(document).ready(function() {
+    const circularProgress = $(".circular-progress");
+    const progressValues = $(".progress-value");
+    const skills = [0, 0, 0, 0]; 
+    const speed = 50;
+    const progressEndValues = [90, 70, 80, 60]; 
 
-    // Function to calculate the skill color
-    function calculateSkillColor(value) {
-        // Define color ranges and corresponding colors
-        var colorRanges = [
-            { min: 0, max: 30, color: "#FF5733" },  // Red
-            { min: 30, max: 60, color: "#FFA933" }, // Orange
-            { min: 60, max: 100, color: "#33FF33" } // Green
-        ];
-
-        // Find the appropriate color range based on the skill value
-        for (var i = 0; i < colorRanges.length; i++) {
-            if (value >= colorRanges[i].min && value <= colorRanges[i].max) {
-                return colorRanges[i].color;
+    function updateProgress() {
+        for (let i = 0; i < circularProgress.length; i++) {
+            if (skills[i] < progressEndValues[i]) {
+                skills[i]++;
+                progressValues.eq(i).text(skills[i] + "%");
+                circularProgress.eq(i).css("background", "conic-gradient(#1797ff " + skills[i] * 3.6 + "deg, #ededed 0deg)");
             }
         }
-
-        return "#e0e0e0"; // Default color if no range matches
+        
+        const allSkillsComplete = skills.every(function(skill, index) {
+            return skill === progressEndValues[index];
+        });
+        if (allSkillsComplete) {
+            clearInterval(progressInterval);
+        }
     }
+
+    const progressInterval = setInterval(updateProgress, speed);
 });
