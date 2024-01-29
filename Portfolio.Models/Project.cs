fc8f7fa3e49f65275e422d0987770e2fda67178c
Kaleb Garrett <kalebagarrett@gmail.com>
2024-01-29 17:47:17 -0500

    Created a db context and project model

---
+++ Portfolio.Models/Project.cs
@@ -0,0 +1,11 @@
+﻿namespace Portfolio.Models;
+
+public class Project
+{
+    public int Id { get; set; }
+    public string ProjectName { get; set; }
+    public string Description { get; set; }
+    public string Challenges  { get; set; }
+    public string Solutions  { get; set; }
+    public string WhatILearned  { get; set; }
+}
---
+++ Portfolio.Models/ProjectDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Portfolio.Models;
+
+public class ProjectDbContext : DbContext
+{
+    public ProjectDbContext(DbContextOptions<ProjectDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Project> Projects { get; set; }
+}
